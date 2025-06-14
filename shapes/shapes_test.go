69fd4eee6c5f58e5f7c15eee7e1e8784cdcbb0a8
Matt Ho <contact@mattbho.com>
2025-06-14 16:35:39 -0700

    intro to methods

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -3,22 +3,35 @@ package shapes
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	t.Run("Gets perimeter of shape", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := Perimeter(rectangle)
-		want := 40.0
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-	t.Run("Gets area of shape", func(t *testing.T) {
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	t.Run("Rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12.0, 6.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 72.0
 
 		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+			// g will print precise decimal
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("Circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			// g will print precise decimal
+			t.Errorf("got %g want %g", got, want)
 		}
 	})
 }
