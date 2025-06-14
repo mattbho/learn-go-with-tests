82d371598c342bb8c224dd32a4b11c8d6470aef9
Matt Ho <contact@mattbho.com>
2025-06-14 16:44:16 -0700

    shape interface, into table tests

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -13,25 +13,38 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
+	// checkArea := func(t testing.TB, shape Shape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		// g will print precise decimal
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
 
-		if got != want {
-			// g will print precise decimal
-			t.Errorf("got %g want %g", got, want)
+	// t.Run("Rectangles", func(t *testing.T) {
+	// 	rectangle := Rectangle{12.0, 6.0}
+	// 	checkArea(t, rectangle, 72.0)
+
+	// })
+
+	// t.Run("Circle", func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle, 314.1592653589793)
+	// })
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{Width: 12, Height: 6}, 72.0},
+		{Circle{Radius: 10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
-	})
-
-	t.Run("Circle", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			// g will print precise decimal
-			t.Errorf("got %g want %g", got, want)
-		}
-	})
+	}
 }
